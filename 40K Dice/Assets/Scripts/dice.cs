6afdac4cbda6f1c7ef908b893c6610bf522e2c24
MGW <45159186+MetaGameWorks@users.noreply.github.com>
2020-02-27 23:02:54 -0500

    Initial Upload to GitHub
    uploaded project to github.

---
+++ 40K Dice/Assets/Scripts/dice.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dice : MonoBehaviour
+{
+    private GameObject numOfAttacks;
+    private GameObject numToHit;
+    private GameObject numToWound;
+    public int[] hitDiePool;
+    public int[] woundDiePool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int Roll(int attacks, int hitReq, int woundReq)
+    {
+        hitDiePool = new int[6];
+        woundDiePool = new int[6];
+        int numOfHits = 0;
+        int numOfWounds = 0;
+
+        for(int i = 0; i <= attacks; i++)
+        {
+            hitDiePool[Random.Range(0, 6)]++;
+        }
+
+        for(int i = 5; i >= (hitReq - 1); i--)
+        {
+            numOfHits += hitDiePool[i];
+        }
+
+        for(int i = 0; i <= numOfHits; i++)
+        {
+            woundDiePool[Random.Range(0, 6)]++;
+        }
+
+        for (int i = 5; i >= (woundReq - 1); i--)
+        {
+            numOfWounds += woundDiePool[i];
+        }
+
+        return numOfWounds;
+    }
+}
