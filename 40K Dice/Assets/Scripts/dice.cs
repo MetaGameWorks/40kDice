7606fb0317a7cd84ee567afebbf83c45eae71388
MGW <45159186+MetaGameWorks@users.noreply.github.com>
2020-02-29 16:33:58 -0500

    finished rolling dice script
    rolling dice script was tested and is working.

--- 40K Dice/Assets/Scripts/dice.cs
+++ 40K Dice/Assets/Scripts/dice.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class dice : MonoBehaviour
 {
-    private GameObject numOfAttacks;
-    private GameObject numToHit;
-    private GameObject numToWound;
+    //private GameObject numOfAttacks;
+    //private GameObject numToHit;
+    //private GameObject numToWound;
+    public int attacks = 0;
+    public int hit = 0;
+    public int wound = 0;
+    public bool explodeHit = false;
     public int[] hitDiePool;
+    public int[] extraDiePool;
     public int[] woundDiePool;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -19,36 +25,64 @@ public class dice : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Roll(attacks, hit, wound);
+        }
     }
 
-    public int Roll(int attacks, int hitReq, int woundReq)
+    public void Roll(int attacks, int hitReq, int woundReq)
     {
         hitDiePool = new int[6];
         woundDiePool = new int[6];
         int numOfHits = 0;
         int numOfWounds = 0;
 
-        for(int i = 0; i <= attacks; i++)
+        for(int i = 1; i <= attacks; i++)
         {
             hitDiePool[Random.Range(0, 6)]++;
         }
 
-        for(int i = 5; i >= (hitReq - 1); i--)
+        Debug.Log("Hits - 6: " + hitDiePool[5] + " / 5: " + hitDiePool[4] + " / 4: " + hitDiePool[3] + " / 3: " + hitDiePool[2] + " / 2: " + hitDiePool[1] + " / 1: " + hitDiePool[0]);
+
+        if(explodeHit)
+        {
+            if(hitDiePool[5] != 0)
+            {
+                int extraHits = hitDiePool[5];
+                extraDiePool = new int[6];
+
+                for(int i = 1; i <= attacks; i++)
+                {
+                    extraDiePool[Random.Range(0, 6)]++;
+                }
+
+                for (int i = 5; i >= (hitReq - 1); i--)
+                {
+                    numOfHits += hitDiePool[i];
+                }
+
+                Debug.Log("Exploding Hits - 6: " + extraDiePool[5] + " / 5: " + extraDiePool[4] + " / 4: " + extraDiePool[3] + " / 3: " + extraDiePool[2] + " / 2: " + extraDiePool[1] + " / 1: " + extraDiePool[0]);
+            }
+        }
+
+        for (int i = 5; i >= (hitReq-1); i--)
         {
             numOfHits += hitDiePool[i];
         }
 
-        for(int i = 0; i <= numOfHits; i++)
+        for(int i = 1; i <= numOfHits; i++)
         {
             woundDiePool[Random.Range(0, 6)]++;
         }
 
-        for (int i = 5; i >= (woundReq - 1); i--)
+        Debug.Log("Wounds - 6: " + woundDiePool[5] + " / 5: " + woundDiePool[4] + " / 4: " + woundDiePool[3] + " / 3: " + woundDiePool[2] + " / 2: " + woundDiePool[1] + " / 1: " + woundDiePool[0]);
+
+        for (int i = 5; i >= (woundReq-1); i--)
         {
             numOfWounds += woundDiePool[i];
         }
 
-        return numOfWounds;
+        Debug.Log(numOfWounds + " Wounds");
     }
 }
